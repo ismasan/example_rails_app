dd0a7b8ec4e30641f8ade5d2fb9e3b1f2aa17665
Ismael Celis <ismael@ismaelct.com>
2008-12-07 15:30:45 +0000

    deleting

--- app/controllers/comments_controller.rb
+++
@@ -1,9 +0,0 @@
-class CommentsController < ApplicationController
-  resources_controller_for :comments
-  
-  def find_resources
-    bool = params[:all].nil? || params[:all] == 1
-    @comments = resource_service.published_only(bool).paginate(:page=>params[:page])
-  end
-  
-end
